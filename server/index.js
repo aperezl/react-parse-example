72041b93279cbc64d20321af246d285045162100
Antonio Manuel Perez Lopez <antonio.perez@podsystem.com>
2021-04-05 23:45:39 +0200

    add parse dashboard

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from  'express'
 import { ParseServer } from 'parse-server'
-
+import ParseDashboard from 'parse-dashboard'
 
 const app = express()
 
@@ -13,6 +13,24 @@ const api = new ParseServer({
   serverURL: 'http://localhost:1337/parse'
 })
 
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: 'http://localhost:1337/parse',
+      appId: 'myAppId',
+      masterKey: 'myMasterKey',
+      appName: 'MyApp'
+    }
+  ],
+  users: [
+    {
+      user: 'admin',
+      pass: 'admin',
+    }
+  ]
+})
+
 app.use('/parse', api)
+app.use('/dashboard', dashboard)
 
 app.listen(1337, () => console.log('parse-server-example running on port 1337.'))
